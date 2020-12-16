118522e6f53baec23f82a74505b0d2fbc86ce411
Nathan Krasney <natankrasney@gmail.com>
2020-12-16 08:47:48 +0200

    string

---
+++ casting.py
@@ -0,0 +1,6 @@
+a = 1
+b = int(1)
+if (a == b):
+    print('equal')
+
+ 
---
+++ string.py
@@ -0,0 +1,10 @@
+str = "hello"
+print(str[0])
+
+for s in str:
+    print (s)
+
+print(len(str))
+
+if "ell" in str:
+    print('exist')
