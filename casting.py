4ffadab32f3dc768a21d5b2bb3013beeece9c2d9
Nathan Krasney <natankrasney@gmail.com>
2020-12-16 10:01:44 +0200

    collections

--- casting.py
+++ casting.py
@@ -2,5 +2,7 @@ a = 1
 b = int(1)
 if (a == b):
     print('equal')
+    
+print (bool(1),bool(0))    
 
  
---
+++ collections.py
@@ -0,0 +1,24 @@
+# list
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+print(mylist[0])
+for it in mylist:
+     print(it)
+
+# tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[0]) # print apple
+
+# set
+myset = {3,4,5}
+print (myset)
+for it in myset:
+    print(it)
+
+# dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
