90f9866f4d3c71dc8d081ce62e41a1af3278f313
Nathan Krasney <natankrasney@gmail.com>
2020-12-17 09:46:11 +0200

    functions

---
+++ loops.py
@@ -0,0 +1,10 @@
+# --- for 
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+  print(fruit)
+
+# --- while
+i = 10
+while i > 0:
+  print (i)
+  i -= 1
---
+++ use-module.py
@@ -0,0 +1,8 @@
+import functions
+from functions import diff 
+
+s = functions.sum(11,22)
+print(s)
+
+d = diff(22,11)
+print(d)
