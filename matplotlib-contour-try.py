a604137dcdfe6b72528ff47814b82857232e97e4
Nathan Krasney <natankrasney@gmail.com>
2020-12-30 11:34:00 +0200

    contour plot

---
+++ matplotlib-contour-try.py
@@ -0,0 +1,19 @@
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+delta = 0.01
+x = np.arange(-3.0, 3.0, delta)
+y = np.arange(-3.0, 3.0, delta)
+X, Y = np.meshgrid(x, y)
+Z = X**2 + Y**2
+
+fig, ax = plt.subplots()
+CS = ax.contour(X, Y, Z)
+ax.clabel(CS, inline=True, fontsize=10)
+ax.set_title('Simplest default with labels')
+
+
+plt.show()
