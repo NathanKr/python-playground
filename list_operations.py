964420937fbfa5ec222bd25d93dbc844b02d7beb
Nathan Krasney <natankrasney@gmail.com>
2021-03-04 18:36:14 +0200

    list operations

---
+++ list_operations.py
@@ -0,0 +1,13 @@
+l1 = [1,2,3]
+l2 = [4,5,6]
+l3 = [1,2,3]
+
+
+# number of items in list
+print(f"number of items in l1 : {len(l1)}")
+
+# compare lists , == compare the data element wise
+print(l1 == l1)
+print(l1 == l3)
+print(l1 == l2)
+
