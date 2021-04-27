8770241122e2172d34d126ed995e1a7c6f276cdb
Nathan Krasney <natankrasney@gmail.com>
2021-04-27 19:27:16 +0300

    static type check

---
+++ static_type_check.py
@@ -0,0 +1,12 @@
+
+def max(num1 : int , num2 : int) -> int:
+    if num1 > num2:
+        return num1
+
+    return num2        
+
+
+# max(1,"3")    this will cause error
+print(max(1,3)) # this is good
+
+x: int = 0
