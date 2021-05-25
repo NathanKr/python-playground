f0c82d6323effbbb185bac809a36f51a997f208f
Nathan Krasney <natankrasney@gmail.com>
2021-05-25 10:20:50 +0300

    ...

--- mutable_object_vs_immutable_object.py
+++ mutable_object_vs_immutable_object.py
@@ -1,3 +1,4 @@
+# all data in python is stored in an object
 # mutable object   : 
 #   - an object whos state CAN be updated after its creation
 #   - e.g. containers like list,dict, set
@@ -6,7 +7,7 @@
 #   - e.g. primitives like string , number
 
 
-# string is immuteable 
+# string is immutable 
 # thus on update a new object is created on new address which str_name point to
 str_name = 'Nathan'
 print(f'id(str_name) : {id(str_name)} , str_name : {str_name}')
@@ -14,7 +15,7 @@ str_name = 'Krasney'
 print(f'id(str_name) : {id(str_name)} , str_name : {str_name}')
 
 # list is mutable
-# thus on update the same object and adderss is used
+# thus on update the same object and address is used
 list = [1,2,3]
 print(f'id(list) : {id(list)} , list : {list}')
 list.append(4)
