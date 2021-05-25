28be0e03c62d3eda48e777bb1e341dfd47f97a3f
Nathan Krasney <natankrasney@gmail.com>
2021-05-25 10:28:06 +0300

    all varaible are objects

---
+++ all_variable_are_objects.py
@@ -0,0 +1,7 @@
+num = 1
+s='abc'
+list = [1,2,3]
+print(f'type(num) : {type(num)}\ntype(s) : {type(s)}\ntype(list) : {type(list)}')
+print(f'type(num).__base__ : {type(num).__base__}\n\
+type(s).__base__ : {type(s).__base__}\n\
+type(list).__base__ : {type(list).__base__}')
